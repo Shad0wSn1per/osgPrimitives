7ebf37604c8b338507c33cf7e4614e85b5273e64
Shad0wSn1per <shadow-sniper@list.ru>
2013-06-16 11:54:01 +0400

    +

---
+++ osgPrimitives/osgPrimitives.cpp
@@ -0,0 +1,19 @@
+// osgPrimitives.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Geometry2D.h"
+
+using namespace osg;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	osgViewer::Viewer viewer;
+	viewer.setUpViewInWindow( 25,25,1024,768 );
+	viewer.getCamera()->setComputeNearFarMode( osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR );
+	viewer.realize();
+	std::string name = Utility::Geometry2D::GeometryFactory::Get().GetName();
+	viewer.run();
+
+	return 0;
+}
+
---
+++ osgPrimitives/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osg/ShapeDrawable>
+#include <osg/Geode>
+#include <osg/Billboard>
+#include <osg/Texture2D>
+#include <osg/PolygonMode>
+#include <osg/TexMat>
+#include <osg/TexGen>
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
